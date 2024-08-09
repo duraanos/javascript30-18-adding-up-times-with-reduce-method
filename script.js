ca8e1ec493f3b0271afaee3ccf62ffdcb5d324f3
duraanos <duran364@anadolu.edu.tr>
2024-08-09 16:23:43 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const timeNodes = document.querySelectorAll('[data-time]');
+
+const seconds = [...timeNodes]
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(':').map(parseFloat);
+    return mins * 60 + secs;
+  })
+  .reduce((total, vidSecs) => total + vidSecs, 0);
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
